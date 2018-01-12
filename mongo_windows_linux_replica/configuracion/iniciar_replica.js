f12c50fe69c63bf145c79f780c120f5867c6e39a
Jairo Rojas <jarojas941018@gmail.com>
2018-01-11 20:05:42 -0500

    Agregando nodo arbitro.

--- mongo_windows_linux_replica/configuracion/iniciar_replica.js
+++ mongo_windows_linux_replica/configuracion/iniciar_replica.js
@@ -4,7 +4,8 @@ cfg={
         {_id:0, host:" localhost:27001"},
         {_id:1, host:" localhost:27002"},
         {_id:2, host:" localhost:27003"},
-        {_id:3, host:" localhost:27004"}
+        {_id:3, host:" localhost:27004"},
+        {_id:4, host:" localhost:27005", arbiterOnly: true }
     ]
 };
 rs.initiate(cfg);
--- mongo_windows_linux_replica/configuracion/start_linux.sh
+++ mongo_windows_linux_replica/configuracion/start_linux.sh
@@ -1,4 +1,8 @@
 #!/bin/bash   
+
+
+cd "/home/centos/replica_mongo/mongo_windows_linux_replica"
+
 echo "Creando carpetas..."
 mkdir data
 mkdir logs
@@ -6,12 +10,14 @@ mkdir data/mongod1
 mkdir data/mongod2
 mkdir data/mongod3
 mkdir data/mongod4
+mkdir data/mongod5
 
 echo "Corriendo procesos mongod..."
 mongod --config configuracion/mongod1_replica1.conf
 mongod --config configuracion/mongod2_replica1.conf
 mongod --config configuracion/mongod3_replica1.conf
 mongod --config configuracion/mongod4_replica1.conf
+mongod --config configuracion/mongod5_replica1.conf
 
 #echo "Esperando 8 segundos..."
 #sleep 8
@@ -27,7 +33,6 @@ echo "Creando usuario Admin..."
 
 echo "Now run:"
 echo "  mongo -u soporteDBAjar -p jarojas94 --authenticationDatabase admin --port 27001"
-
 
 
 #mongo "mongodb://127.0.0.1:27001,127.0.0.1:27002,127.0.0.1:27003/admin?replicaSet=replica1"
