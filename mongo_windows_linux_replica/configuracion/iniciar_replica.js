22afea5d8cbb009964ab16c07af8c5804ce59b5f
Jairo Rojas <jarojas941018@gmail.com>
2018-01-12 12:45:49 -0500

    Ajustando bind ip

--- mongo_windows_linux_replica/configuracion/iniciar_replica.js
+++ mongo_windows_linux_replica/configuracion/iniciar_replica.js
@@ -1,11 +1,11 @@
 cfg={
     _id:"replica1",
     members:[
-        {_id:0, host:" localhost:27001"},
-        {_id:1, host:" localhost:27002"},
-        {_id:2, host:" localhost:27003"},
-        {_id:3, host:" localhost:27004"},
-        {_id:4, host:" localhost:27005", arbiterOnly: true }
+        {_id:0, host:" 10.20.0.168:27001"},
+        {_id:1, host:" 10.20.0.168:27002"},
+        {_id:2, host:" 10.20.0.168:27003"},
+        {_id:3, host:" 10.20.0.168:27004"},
+        {_id:4, host:" 10.20.0.168:27005", arbiterOnly: true }
     ]
 };
 rs.initiate(cfg);
